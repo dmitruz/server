696d456d69326f8a508dd97178cc19d2d3ce2861
dmitruz <dmitruz970@gmail.com>
2022-05-25 17:38:50 +0300

    bjkbkjbkjvbjvb

---
+++ http-server/app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const products = require('./products');
+
+
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/' && method === 'GET') {
+        console.log('in /');
+
+        res.write('Hello world');
+
+    } else if(url === '/products' && method === 'GET') {
+        const all = await products.getAll();
+        res.write(JSON.stringify(all));
+    }
+    res.end();
+});
+
+
+const PORT = 8081;
+server.listen(PORT, () => {
+    console.log(`Server in on ${PORT}`);
+})
