b32bca5f86a3fbdb6d06b28da52d9b725e7260a1
dmitruz <dmitruz970@gmail.com>
2022-06-07 17:31:26 +0300

    dvdgrg

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
-const DB_HOST = 'mongodb+srv://testuser:840270aaa@cluster0.mgfa48c.mongodb.net/products?retryWrites=true&w=majority';
+const DB_HOST = 'mongodb+srv://testuser:<840270aaa>@cluster0.mgfa48c.mongodb.net/products?retryWrites=true&w=majority';
 async function main() {
   await mongoose.connect(DB_HOST);
 }
