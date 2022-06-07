bc69b40c9adcb34f1fa63784c2e8776f20a666de
dmitruz <dmitruz970@gmail.com>
2022-06-07 21:58:46 +0300

    2

--- product-rest-api/services/auth.service.js
+++ product-rest-api/services/auth.service.js
@@ -1,5 +1,5 @@
 const {User} = require('../models/user');
-const {createError} = require("../helpers/errors");
+const {createError} = require("../helpers/error");
 const bcrypt = require('bcryptjs');
 
 
