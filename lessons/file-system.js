d51817605c53a1976a7ad0eaa0689a9e9fa07033
dmitruz <dmitruz970@gmail.com>
2022-06-29 21:47:20 +0300

    dgedg

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -20,3 +20,30 @@ const appendFileAsync = async (path, data) => {
         resolve()
     }))
 }
+
+const readFile = async (path) => {
+    return new Promise((resolve, reject) => fs.readFile(path, {encoding: 'utf-8'}, (err) => {
+        if (err) {
+            return reject(err.message)
+        }
+        resolve(data)
+    }))
+}
+
+const removeFileAsync  = async (path) => {
+    return new Promise((resolve, reject) => fs.rm(path, (err) => {
+        if (err) {
+            return reject(err.message)
+        }
+        resolve()
+    }))
+}
+
+removeFileAsync(path.resolve(__dirname, 'test.txt'))
+.then(() => console.log('file was removed'))
+
+//writeFileAsync(path.resolve(__dirname, 'test.txt'), 'data')
+//.then(() => appendFileAsync('123'))
+//then(() => readFile(path.resolve(__dirname, 'test.txt')))
+//.then(data => console.log(data))
+//.catch(err => console.log('err'))
