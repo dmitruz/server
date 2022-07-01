048718c0e96d2968110280446ab6dec4671829e7
dmitruz <dmitruz970@gmail.com>
2022-07-01 18:29:35 +0300

    NBBJB

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-const doteenv = require('doteenv')
-doteenv.config()
+// const doteenv = require('doteenv')
+// doteenv.config()
 
-console.log(process.env.PORT);
-console.log(process.env.NODE_ENV);
+// console.log(process.env.PORT);
+// console.log(process.env.NODE_ENV);
+
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const EventEmitter = require('events');
+const emitter = new EventEmitter()
+
+class Router {
+    constructor() {
+        this.endpoint = {}
+    }
+
+    request(method = "GET", path, handler) {
+        if(!this.endpoints[path]) {
+            this.endpoints[path] = {}
+        }
+    const endpoint = this.endpoints[path];
+
+    if(endpoint[method]) {
+        throw new Error(`[${method}] by address ${path} is already exist`)
+    }
+
+    endpoint[method] = handler
+
+    emitter.on(`[${path}]:[${method}]`, (req, res) => {
+        handler(req, res)
+    })
+    }
+}
+
+get(path, handler) {
+    this.request('GET', path, handler);
+}
+
+put(path, handler) {
+    this.request('PUT', path, handler);
+}
+
+post(path, handler) {
+    this.request('POST', path, handler);
+}
+
+delete(path, handler) {
+    this.request('DELETE', path, handler);
+}
+
+const router = new Router()
+router.get('/users', (req, res) => {
+    res.send('YOU SEND REQUEST TO /USERS')
+})
+const server = http.createServer((res, req) => {
+
+res.end(req.url)
+})
+
+server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
--- lessons/stream.js
+++ lessons/stream.js
@@ -13,7 +13,23 @@ const path = require('path')
 //     console.log(data)
 // })
 
-const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
-stream.on('data', (chunk) => {
-    console.log(chunk)
-})
+// const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
+// stream.on('data', (chunk) => {
+//     console.log(chunk)
+// })
+
+// const writableStream = fs.createWriteStream(path.resolve(__dirname, 'test2.txt'))
+// for (let i = 0; i < 20; i++) {
+//     writableStream.write(i + '\n');
+// }
+// writableStream.end()
+
+
+// otpravka faila polzovatelu
+
+// const http = require('http');
+
+// http.createServer((req, res) => {
+//     const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
+//     stream.pipe(res)
+// })
